19b69294d7b66f607e31affa055e2ce6a0571af4
bitcoinnickel <48751960+bitcoinNickel@users.noreply.github.com>
2021-10-13 21:26:42 +0800

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -152,7 +152,7 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("node1.walletbuilders.com");
+        vSeeds.emplace_back("dnsseed.btcnickel.com");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,54);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,53);
